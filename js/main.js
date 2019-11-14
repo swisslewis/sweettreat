a41947f6a31073c701c29e44843cbbc65059e6e2
Christopher Lewis <932400356@col573-l13-15.local>
2019-11-14 18:35:19 +1300

    updating all files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+// Get DOM elements
+        
+const EL_SEARCH_BOX = $("#search-box");
+const EL_FLAVORS_ICECREAM = $("#flavors-icecream");
+const EL_FLAVORS_DONUTS = $("#flavors-donuts");
+const EL_FLAVORS_YOGHURT = $("#flavors-yoghurt");
+const EL_QUOTE_BOX = $("#quote-box");
+const EL_QUOTE_BUTTON = $("#quote-button");
+
+
+
+
+
+
+
+
+
+});
